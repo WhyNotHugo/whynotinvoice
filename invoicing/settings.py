4b43983f3edef3a671f6f9b84d5a2c0007c47a2f
Hugo Osvaldo Barrera <hugo@barrera.io>
2017-10-03 02:15:48 -0300

    Use a different SECRET_KEY in production

--- invoicing/settings.py
+++ invoicing/settings.py
@@ -8,12 +8,11 @@ from envparse import env
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jmp99uRoeDazT9sCKyJwmBNiBKkoYlfwv44aCVjacIudUlTk'
+SECRET_KEY = env(
+    'SECRET_KEY',
+    cast=str,
+    default='jmp99uRoeDazT9sCKyJwmBNiBKkoYlfwv44aCVjacIudUlTk',
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', cast=bool, default=True)
