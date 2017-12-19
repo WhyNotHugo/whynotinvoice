d778cdb8f7592b5b2086a6d672582d34cea28ee4
Hugo Osvaldo Barrera <hugo@barrera.io>
2017-12-19 10:28:22 -0300

    Fix accidentally broken admin URLs

--- invoicing/urls.py
+++ invoicing/urls.py
@@ -6,7 +6,7 @@ from django.views.static import serve
 from invoicing import views
 
 urlpatterns = [
-    path('/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path(
         '^invoices/pdf/<int:pk>$',
         views.SafeReceiptPDFView.as_view(),
