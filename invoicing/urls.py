0796e267cf73d4c38a0458a372fd25b8a70d6523
Hugo Osvaldo Barrera <hugo@barrera.io>
2021-01-06 19:40:11 +0100

    Update some mis-migrates URLs

--- invoicing/urls.py
+++ invoicing/urls.py
@@ -8,12 +8,12 @@ from invoicing import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(
-        '^invoices/pdf/<int:pk>$',
+        'invoices/pdf/<int:pk>',
         views.SafeReceiptPDFView.as_view(),
         name='receipt_pdf_view',
     ),
     path(
-        '^media/<str:path>$',
+        'media/<str:path>',
         serve,
         {'document_root': settings.MEDIA_ROOT},
     ),
